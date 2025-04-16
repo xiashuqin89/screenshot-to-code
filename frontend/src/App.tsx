733f4c0c52b80eb43a635daa9f976d3f086d3bae
neolei <neolei@tencent.com>
2025-04-16 17:12:27 +0800

    base update 0.3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -345,7 +345,7 @@ function App() {
           onOpenChange={handleTermDialogOpenChange}
         />
       )}
-      <div className="lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:80 lg:flex-col">
+      <div className="lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:w-96 lg:flex-col">
         <div className="flex grow flex-col gap-y-2 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:bg-zinc-950 dark:text-white">
           {/* Header with access to settings */}
           <div className="flex items-center justify-between mt-4 mb-4">
