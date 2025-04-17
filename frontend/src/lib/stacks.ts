5683af040b74564ec5511aae8f630bfe76f34114
neolei <neolei@tencent.com>
2025-04-17 15:02:59 +0800

    base update 0.4

--- frontend/src/lib/stacks.ts
+++ frontend/src/lib/stacks.ts
@@ -3,11 +3,11 @@
 export enum Stack {
   HTML_CSS = "html_css",
   SVG = "svg",
-  // HTML_TAILWIND = "html_tailwind",
-  // REACT_TAILWIND = "react_tailwind",
-  // BOOTSTRAP = "bootstrap",
-  // VUE_TAILWIND = "vue_tailwind",
-  // IONIC_TAILWIND = "ionic_tailwind",
+  HTML_TAILWIND = "html_tailwind",
+  REACT_TAILWIND = "react_tailwind",
+  BOOTSTRAP = "bootstrap",
+  VUE_TAILWIND = "vue_tailwind",
+  IONIC_TAILWIND = "ionic_tailwind",
 }
 
 export const STACK_DESCRIPTIONS: {
@@ -15,9 +15,9 @@ export const STACK_DESCRIPTIONS: {
 } = {
   html_css: { components: ["HTML", "CSS"], inBeta: false },
   svg: { components: ["SVG"], inBeta: true },
-  // html_tailwind: { components: ["HTML", "Tailwind"], inBeta: false },
-  // react_tailwind: { components: ["React", "Tailwind"], inBeta: false },
-  // bootstrap: { components: ["Bootstrap"], inBeta: false },
-  // vue_tailwind: { components: ["Vue", "Tailwind"], inBeta: true },
-  // ionic_tailwind: { components: ["Ionic", "Tailwind"], inBeta: true },
+  html_tailwind: { components: ["HTML", "Tailwind"], inBeta: false },
+  react_tailwind: { components: ["React", "Tailwind"], inBeta: false },
+  bootstrap: { components: ["Bootstrap"], inBeta: false },
+  vue_tailwind: { components: ["Vue", "Tailwind"], inBeta: true },
+  ionic_tailwind: { components: ["Ionic", "Tailwind"], inBeta: true },
 };
